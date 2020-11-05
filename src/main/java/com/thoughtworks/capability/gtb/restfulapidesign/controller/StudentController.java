3595cc50a67f1c19cd2c7394b2f7cc22b3b68a33
g992987642 <992987642@qq.com>
2020-11-05 22:20:00 +0800

    [guhao]: add repository

--- src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/controller/StudentController.java
+++ src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/controller/StudentController.java
@@ -3,8 +3,7 @@ package com.thoughtworks.capability.gtb.restfulapidesign.controller;
 import com.thoughtworks.capability.gtb.restfulapidesign.StudentService;
 import com.thoughtworks.capability.gtb.restfulapidesign.dto.Student;
 import com.thoughtworks.capability.gtb.restfulapidesign.dto.StudentGroup;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,35 +15,32 @@ import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
-// TODO GTB-知识点: - 构造器注入时可以省略@Autowired
-@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@AllArgsConstructor
 public class StudentController {
 
     private final StudentService studentService;
 
     @GetMapping("/students")
-    // TODO GTB-知识点: - 没有使用泛型
-    public ResponseEntity getStudents() {
+    public ResponseEntity<List<Student>> getStudents() {
         List<Student> students = studentService.getStudents();
         return ResponseEntity.ok().body(students);
     }
 
 
-    // TODO GTB-知识点: - 违反Restful实践, url不合理
-    @PostMapping("/student/groups")
+    @PostMapping("/students/groups")
     public ResponseEntity groupStudentRandomly() throws URISyntaxException {
         studentService.groupStudentRandomly();
-        return ResponseEntity.created(new URI("student/groups")).build();
+        return ResponseEntity.created(new URI("students/groups")).build();
 
     }
-    @GetMapping("/student/groups")
-    public ResponseEntity getStudentsGroups(){
+    @GetMapping("/students/groups")
+    public ResponseEntity<List<StudentGroup>> getStudentsGroups(){
         List<StudentGroup> studentGroups = studentService.getStudentGroups();
         return ResponseEntity.ok().body(studentGroups);
 
     }
 
-    @PostMapping("/student")
+    @PostMapping("/students")
     public ResponseEntity addStudent(@RequestBody Student student) throws URISyntaxException {
         studentService.addStudent(student);
         return ResponseEntity.created(new URI("/students")).build();
--- src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/impl/StudentServiceImpl.java
+++ src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/impl/StudentServiceImpl.java
@@ -3,54 +3,29 @@ package com.thoughtworks.capability.gtb.restfulapidesign.impl;
 import com.thoughtworks.capability.gtb.restfulapidesign.StudentService;
 import com.thoughtworks.capability.gtb.restfulapidesign.dto.Student;
 import com.thoughtworks.capability.gtb.restfulapidesign.dto.StudentGroup;
+import com.thoughtworks.capability.gtb.restfulapidesign.repository.StudentRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
-import java.util.concurrent.CopyOnWriteArrayList;
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
 
 @Service
 public class StudentServiceImpl implements StudentService {
 
-    // TODO GTB-知识点: - 没有使用钻石操作符
-    public static CopyOnWriteArrayList<Student> studentDB=new CopyOnWriteArrayList<Student>();
+ private final StudentRepository studentRepository =new StudentRepository();
 
-    public static CopyOnWriteArrayList<StudentGroup> studentGroups=new CopyOnWriteArrayList<StudentGroup>();
 
-    // TODO GTB-知识点: - 不符合三层架构，缺少用于保存数据的Repository层
-    public static void initStudentDB(){
-        studentDB.clear();
-        studentDB.add(new Student(1,"成吉思汗"));
-        studentDB.add(new Student(2,"鲁班七号"));
-        studentDB.add(new Student(3,"太乙真人"));
-        studentDB.add(new Student(4,"钟无艳"));
-        studentDB.add(new Student(5,"花木兰"));
-        studentDB.add(new Student(6,"雅典娜"));
-        studentDB.add(new Student(7,"芈月"));
-        studentDB.add(new Student(8,"白起"));
-        studentDB.add(new Student(9,"刘禅"));
-        studentDB.add(new Student(10,"庄周"));
-        studentDB.add(new Student(11,"马超"));
-        studentDB.add(new Student(12,"刘备"));
-        studentDB.add(new Student(13,"哪吒"));
-        studentDB.add(new Student(14,"大桥"));
-        studentDB.add(new Student(15,"蔡文姬"));
-    };
-    public StudentServiceImpl() {
-        initStudentDB();
-        // TODO GTB-完成度: - 应该在点击分组的时候才生成组
-        initStudentGroups();
-    }
 
     public void initStudentGroups(){
-        studentGroups.clear();
-        // TODO GTB-工程实践: - Magic Number
-        int groups=6;
-        // TODO GTB-知识点: - 以下代码可以使用Java8 Stream简化
-        for (int i=0;i<groups;i++){
-        studentGroups.add(new StudentGroup((1+i)+" 组"));
+        StudentRepository.studentGroups.clear();
+        int groupCounts=StudentRepository.GROUPCOUNTS;
+        for (int i=0;i<groupCounts;i++){
+            StudentRepository.studentGroups.add(new StudentGroup((1+i)+" 组"));
         }
-        Collections.sort(studentGroups, new Comparator<StudentGroup>() {
+        Collections.sort(    StudentRepository.studentGroups, new Comparator<StudentGroup>() {
             @Override
             public int compare(StudentGroup o1, StudentGroup o2) {
                 return o1.getGroupName().compareTo(o2.getGroupName());
@@ -59,63 +34,39 @@ public class StudentServiceImpl implements StudentService {
     }
     @Override
     public List<Student> getStudents() {
-        ArrayList<Student> students = new ArrayList<>();
-        students.addAll(studentDB);
-        sortListById(students);
+        List<Student> students = new ArrayList<>();
+        students.addAll(StudentRepository.studentDB);
+        students.sort(Comparator.comparing(Student::getId));
         return students;
     }
 
     @Override
     public void groupStudentRandomly() {
         initStudentGroups();
-        ArrayList<Student> students = randomSortStudent();
-        // TODO GTB-工程实践: - 列表遍历推荐使用for循环
-        int i=0;
-        while (i<students.size()){
-            StudentGroup studentGroup = studentGroups.get(i % studentGroups.size());
+        List<Student> students = randomSortStudent();
+        for(int i=0;i<students.size();i++){
+            StudentGroup studentGroup = StudentRepository.studentGroups.get(i % StudentRepository.studentGroups.size());
             studentGroup.add(students.get(i));
-            sortListById(studentGroup.getStudents());
-            i++;
+            studentGroup.getStudents().sort(Comparator.comparing(Student::getId));
         }
-        System.out.println(studentGroups.toString());
     }
 
-    //Knuth-Durstenfeld Shuffle
-    // TODO GTB-知识点: - 打乱顺序可以了解下Collections.shuffle()
-    public ArrayList<Student> randomSortStudent(){
-        ArrayList<Student> result = new ArrayList<>();
-        Student[] randomStudents =new Student[studentDB.size()];
-        randomStudents= studentDB.toArray( randomStudents );
-        int studentCount=randomStudents.length;
-        for(int i=0;i<studentCount;i++){
-            int r =new Random().nextInt(studentCount-i);
-            Student temp=randomStudents[i];
-            randomStudents[i]= randomStudents[r];
-            randomStudents[r]=temp;
-        }
-        Collections.addAll(result,randomStudents);
+    public List<Student> randomSortStudent(){
+        Random rand=new Random(47);
+        List<Student> result = new ArrayList<>();
+        result.addAll(StudentRepository.studentDB);
+        Collections.shuffle(result);
         return result;
-    }
-    public void sortListById(List<Student> list){
-        // TODO GTB-知识点: - 了解下Comparator的静态方法
-        Collections.sort(list, new Comparator<Student>() {
-            @Override
-            public int compare(Student o1, Student o2) {
-                return o1.getId()-o2.getId();
-            }
-        });
     }
 
     @Override
     public List<StudentGroup> getStudentGroups() {
-        return studentGroups;
+        return StudentRepository.studentGroups;
     }
 
     @Override
-    // TODO GTB-工程实践: - 计算id的方式不够健壮，可以使用字段保存最大id
     public void addStudent(Student student) {
-        int size = studentDB.size();
-        student.setId(size+1);
-        studentDB.add(student);
+        student.setId(studentRepository.studentNumber.incrementAndGet());
+        StudentRepository.studentDB.add(student);
     }
 }
